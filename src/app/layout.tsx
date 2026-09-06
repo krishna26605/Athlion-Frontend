c95f616447d09af9d78303b453b0c2f4841d9b8f
krishnanikamdelxn <krishnanikam.delxn@gmail.com>
2026-09-06 10:55:47 +0530

    GLOBE SYMBOL REPLACED

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,10 +71,15 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: '/favicon.ico' },
       { url: '/FINAL-ATH-LOGO.png', type: 'image/png' },
+      { url: '/icon-48x48.png', sizes: '48x48', type: 'image/png' },
+      { url: '/icon-192x192.png', sizes: '192x192', type: 'image/png' },
     ],
     shortcut: '/FINAL-ATH-LOGO.png',
-    apple: '/FINAL-ATH-LOGO.png',
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
   },
   manifest: "/manifest.json",
   appleWebApp: {
